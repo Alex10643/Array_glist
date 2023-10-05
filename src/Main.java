1369f47845fdac7dc2bc1889c0f2eacc8520cb55
Alex <alex.shalimovlol@gmail.com>
2023-10-05 23:23:04 +0300

    Added MyArrayList.java.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyArrayList MyNewBros = new MyArrayList();
+        MyNewBros.add("Vovan");
+        MyNewBros.add("Sasha");
+        MyNewBros.add("Jonh Torchovski");
+        MyNewBros.add("Victor Korneplod");
+        MyNewBros.add("The Death");
+        MyNewBros.add("Natasha");
+        MyNewBros.add("Masha");
+        MyNewBros.add("Pasha");
+        MyNewBros.add("Sergey");
+        MyNewBros.add("Vovan's clone");
+        MyNewBros.add("Nitrostas");
+        MyNewBros.add("Dumb the Bob one");
+        MyNewBros.add("Dumpling Eater");
+        for (int i = 0; i < MyNewBros.getSize(); i++) {
+            System.out.println(MyNewBros.get(i));
+        }
     }
 }
+
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class MyArrayList {
+public class MyArrayList {
+    private String[] array = new String[10];
+    private int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    public String[] getArray() {
+        return array;
+    }
+
+    void add(String str) {
+        if (size == array.length) {
+            String[] newArray = new String[array.length*2];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+        array[size] = str;
+        size++;
+    }
+
+    void remove (int index) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+            }
+        }
+        size--;
+    }
+
+    void remove (String name) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (name.equals(array[i])) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    String get(int index) {
+        if (index >= 0 && index < size) {
+            return array[index];
+        } else {
+            return "No element with this name";
+        }
+    }
 }
